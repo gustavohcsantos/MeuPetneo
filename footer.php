31fe90a02124d420a2b5809baa43261fd0b145b1
Gustavo Henrique da Costa Santos <gustavohcsantos@gmail.com>
2021-12-27 00:38:44 -0300

    Atualizado o footer.php de 'Feito por: Gustavo Henrique' para 'Daora Soluções Web' e linkando o mesmo com cerquilha, aguardando o link da página real; Atualizado a folha de estilo na área do FOOTER referente ao identificador 'p#footer a'.

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <?php
     echo '
             <footer>
-                <p id="footer">Todos os direitos reservados à Meu Petneo 2021<br />Feito por: Gustavo Henrique</p>
+                <p id="footer">Todos os direitos reservados à Meu Petneo 2021<br /><a href="#" title="Conheça esta empresa, ela é Daora!" alt="Quer conhecer sobre a empresa que construiu este site? Conheça clicando neste link.">Daora Soluções Web</a></p>
             </footer>
         </body>
     </html>
