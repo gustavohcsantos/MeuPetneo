71eca40d410a38cfe96cde87351dc5f4356cbe59
Gustavo Henrique da Costa Santos <gustavohcsantos@gmail.com>
2021-12-27 23:20:19 -0300

    Adicionando o campo onde mostrará o resultado do cálculo na página index.php; Construído parcialmente a função JavaScript calcular_tamanho_roupa()

--- index.php
+++ index.php
@@ -10,15 +10,18 @@
             <div class="bloco_formulario_petneo">
                 <div class="formulario_petneo">
                     <form method="POST" action="php/index.php">
-                        <div class="dados_petneo">
-                            <input type="text" name="torax" id="torax" class="entrada_dados_index" placeholder="Tórax (cm)" required="required" /><br />
-                            <input type="text" name="pescoco" id="pescoco" class="entrada_dados_index" placeholder="Pescoço (cm)" required="required" /><br />
-                        </div>
-                        <div class="dados_petneo">
-                        <input type="text" name="comprimento" id="comprimento" class="entrada_dados_index" placeholder="Comprimento (cm)" required="required" /><br />
-                            <input type="text" name="peso" id="peso" class="entrada_dados_index" placeholder="Peso (Kg)" required="required" /><br />
-                        </div>
-                        <input type="submit" name="calcular" id="calcular" value="Calcular"/>
+                        <section id="bloco_secao_input_dados">
+                            <article class="dados_petneo">
+                                <input type="text" name="torax" id="torax" class="entrada_dados_index" placeholder="Tórax (cm)" required="required" /><br />
+                                <input type="text" name="pescoco" id="pescoco" class="entrada_dados_index" placeholder="Pescoço (cm)" required="required" /><br />
+                            </article>
+                            <article class="dados_petneo">
+                                <input type="text" name="comprimento" id="comprimento" class="entrada_dados_index" placeholder="Comprimento (cm)" required="required" /><br />
+                                <input type="text" name="peso" id="peso" class="entrada_dados_index" placeholder="Peso (Kg)" required="required" /><br />
+                            </article>
+                        </section>
+                        <input type="submit" name="calcular" id="calcular" value="Calcular" onclick="calcular_tamanho_roupa_pet()"/><br />
+                        <input type="text" name="resultado" id="resultado" class="resultado_dados_index" placeholder="O RESULTADO APARECERÁ AQUI..." /><br />
                     </form>
                 </div>
             </div>
